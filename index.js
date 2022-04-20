3670493ba7b0d6060f84977b5ce1d1e391b26b75
Winnie Bosibori <winniebosy@gmail.com>
2022-04-20 16:43:22 +0300

    second js file

---
+++ index.js
@@ -0,0 +1,5 @@
+// simple program testing even odd numbers using ternary operator
+
+const input = prompt("Enter a number to determine if is even or odd");
+const output = input % 2 === 0 ? "even" : "odd";
+alert(`The number is ${output}`);
